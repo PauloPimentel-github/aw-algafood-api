1c3fdc851217fb5a62d904ee6763a9970a349b12
PauloPimentel-github <paulo.h.g.pimentel@gmail.com>
2023-04-16 12:47:56 -0300

    Adicionando estrutura inicial do projeto

---
+++ src/main/java/com/phpimentel/awalgafoodapi/AwAlgafoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.phpimentel.awalgafoodapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwAlgafoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwAlgafoodApiApplication.class, args);
+	}
+
+}
