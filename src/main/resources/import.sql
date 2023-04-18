1f8b4c4de62df4c365acf1860d6387d28bdeabc6
PauloPimentel-github <paulo.h.g.pimentel@gmail.com>
2023-04-17 21:32:11 -0300

    Introdução ao JPA e Hibernate > 3.17. Mapeando relacionamento com @ManyToOne

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,6 @@
-insert into cozinhas (nome) values ('Tailandesa');
-insert into cozinhas (nome) values ('Indiana');
+insert into cozinhas (id, nome) values (1, 'Tailandesa');
+insert into cozinhas (id, nome) values (2, 'Indiana');
+
+insert into restaurantes (nome, taxa_frete, cozinha_id) values ('Thai Gourmet', 10, 1);
+insert into restaurantes (nome, taxa_frete, cozinha_id) values ('Thai Delivery', 9.50, 1);
+insert into restaurantes (nome, taxa_frete, cozinha_id) values ('Tuk Tuk Comida Indiana', 15, 2);
